6a57e885b7807e9033f8e9680ba96eb5e427c01c
csalitre <salitrecristian@gmail.com>
2024-08-24 22:48:51 -0400

    data storage is on good path

--- components/data_storage/include/dplock_data_storage.h
+++ components/data_storage/include/dplock_data_storage.h
@@ -1,21 +1,60 @@
-#ifndef DATA_STORAGE_H
-#define DATA_STORAGE_H
+#ifndef DPLOCK_DATA_STORAGE_H
+#define DPLOCK_DATA_STORAGE_H
 
 #include "esp_err.h"
 #include <time.h>
 
 typedef struct {
-    float accelerometer_x;
-    float accelerometer_y;
-    float accelerometer_z;
-    time_t timestamp;
-    float photodiode_value;
-} sensor_data_t;
+    float x;
+    float y;
+    float z;
+    uint64_t timestamp;  // Unix time in milliseconds
+} dplock_accel_data_point_t;
 
-esp_err_t data_storage_inti(void);
-esp_err_t data_storage_save_record(const sensor_data_t* data);
-esp_err_t data_storage_get_all_records(sensor_data_t** data, size_t* const);
-esp_err_t data_storage_clear_all(void);
+typedef struct {
+    uint64_t transit_time;  // elapsed time in milliseconds
+    uint64_t timestamp;
+} dplock_rtc_data_t;
+
+typedef struct {
+    int64_t event_timestamp;
+    bool motor_actuated;
+    bool photodiode_detected_light;
+    bool button_pressed;
+    int16_t accel_x;
+    int16_t accel_y;
+    int16_t accel_z;
+} dplock_breach_event_t;
+
+/* 
+* Initialize the data storage system
+*/
+esp_err_t dplock_data_storage_init(void);
+
+/*
+* Store and retrieve accelerometer data
+*/ 
+esp_err_t dplock_store_accel_data(const dplock_accel_data_point_t *data);
+esp_err_t dplock_get_accel_data(dplock_accel_data_point_t *data);
+
+/*
+* Store and retrieve RTC data
+*/ 
+esp_err_t dplock_store_rtc_data(const dplock_rtc_data_t *data);
+esp_err_t dplock_get_rtc_data(dplock_rtc_data_t *data);
+
+
+/*
+* Store and retrieve breach event data
+*/ 
+esp_err_t dplock_store_breach_event(const dplock_breach_event_t *event);
+esp_err_t dplock_get_breach_event(dplock_breach_event_t *event);
+
+
+/*
+* Clear all stored data
+*/ 
+esp_err_t dplock_clear_data(void);
 
 
 #endif
