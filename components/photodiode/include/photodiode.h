5a0ea2089c84fcfd5a70edc5184d40bae2e06f1e
csalitre <salitrecristian@gmail.com>
2024-09-05 21:37:08 -0400

    making sure code builds and compiles

--- components/photodiode/include/photodiode.h
+++ components/photodiode/include/photodiode.h
@@ -1,4 +1,14 @@
 #ifndef PHOTODIODE_H
 #define PHOTODIODE_H
 
+#include "board_pins.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+
+#define PIN_PHOTODIODE PIN_ADC
+
+void adc_init(void);
+bool adc_calibration_init(adc_unit_t unit, adc_cali_handle_t *out_handle);
+void adc_read(void);
+
 #endif 
