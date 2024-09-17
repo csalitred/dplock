146c6680a5e79ae86c2f3c0f3ea8c98e142df4b4
csalitre <salitrecristian@gmail.com>
2024-09-17 19:07:19 -0400

    in progress needs testing

--- components/i2c_manager/include/i2c_manager.h
+++ components/i2c_manager/include/i2c_manager.h
@@ -1,11 +1,11 @@
 #ifndef I2C_MANAGER_H
 #define I2C_MANAGER_H
 
-#include "esp_err.h"
+#include "stdlib.h"
 
-void i2c_manager_init(void);
-void i2c_manager_read(uint8_t device_address, uint8_t reg_address, uint8_t *data, size_t len);
-void i2c_manager_write(uint8_t device_address, uint8_t reg_address, const uint8_t *data, size_t len);
-void i2c_manager_delete(void);
+void i2c_master_init(void);
+void i2c_master_read_accel(int16_t *x, int16_t *y, int16_t *z, uint8_t reg_addr);
+void i2c_device_uninstall(void);
+void i2c_add_device(uint8_t slave_address);
 
 #endif
