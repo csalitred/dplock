ac4856f0d573c221bae93b6d7211d91b1e629eb9
csalitre <salitrecristian@gmail.com>
2024-09-17 21:43:00 -0400

    renamed pins

--- components/i2c_manager/include/i2c_manager.h
+++ components/i2c_manager/include/i2c_manager.h
@@ -5,7 +5,7 @@
 
 void i2c_master_init(void);
 void i2c_master_read_accel(int16_t *x, int16_t *y, int16_t *z, uint8_t reg_addr);
-void i2c_device_uninstall(void);
 void i2c_add_device(uint8_t slave_address);
+
 
 #endif
